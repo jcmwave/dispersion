b7b1a958048f488ddc408738bd3022b59ab65f3d
gjjring <manley@zib.de>
2021-10-04 15:56:45 +0200

    added pytest features and testing now includes installation script

--- tests/test_material.py
+++ tests/test_material.py
@@ -3,20 +3,25 @@ import numpy as np
 import os
 from dispersion import Material
 from dispersion import Spectrum
-from dispersion import get_config
 
+@pytest.fixture
+def spectrum():
+    spectrum = Spectrum(0.5, unit='um')
+    yield spectrum
 
-spectrum = Spectrum(0.5,unit='um')
+@pytest.fixture
+def root_path():
+    this_dir = os.path.dirname(os.path.abspath(__file__))
+    root_path = os.path.join(this_dir,"..","data")
+    yield root_path
 
-root_path = "../data"
-
-def test_mat_init():
+def test_mat_init(spectrum):
     md = Material(fixed_n=1.0)
     n = md.get_nk_data(spectrum)
     assert np.isclose(np.real(n), 1.0)
     assert np.isclose(np.imag(n), 0.0)
 
-def test_from_yml_file():
+def test_from_yml_file(spectrum, root_path):
     relpath = os.path.join('RefractiveIndexInfo',
                            'data', 'main', 'Ag',
                            'Hagemann.yml')
@@ -28,7 +33,7 @@ def test_from_yml_file():
     assert np.isclose(np.real(n), 0.23805806451612901)
     assert np.isclose(np.imag(n), 3.126040322580645)
 
-def test_from_txt_file():
+def test_from_txt_file(spectrum, root_path):
     relpath = os.path.join('UserData','AlSb.txt')
     filepath = os.path.join(root_path,relpath)
     md = Material(file_path=filepath,
@@ -38,7 +43,7 @@ def test_from_txt_file():
     assert np.isclose(np.real(n), 4.574074754901961)
     assert np.isclose(np.imag(n), 0.4318627450980393)
 
-def test_from_nk_file():
+def test_from_nk_file(spectrum, root_path):
     relpath = os.path.join('Palik','Ag.nk')
     filepath = os.path.join(root_path,relpath)
     md = Material(file_path=filepath,
@@ -49,7 +54,7 @@ def test_from_nk_file():
     assert np.isclose(np.imag(n), 2.917632850241546)
 
 
-def test_from_model():
+def test_from_model(spectrum):
     wp = 8.55 # eV
     loss = 18.4e-3 #eV
     model_kw = {'name':'Drude','parameters':[wp, loss],
