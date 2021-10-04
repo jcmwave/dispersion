ece53bcd39672255eeb4ee9ec57471cb1701839e
gjjring <manley@zib.de>
2021-10-04 16:05:29 +0200

    fixed function naming so script will work

--- src/dispersion/scripts/catalogue_rebuild.py
+++ src/dispersion/scripts/catalogue_rebuild.py
@@ -5,5 +5,8 @@ setup the disperion database file structure and configuration file
 
 from dispersion import rebuild_catalogue
 
-if __name__ == "__main__":
+def main():
     rebuild_catalogue()
+
+if __name__ == "__main__":
+    main()
